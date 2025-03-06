c3ade29730f5b5fd0350bbc2d1e9e722a3e10e6b
matt styles <styles.matt135@gmail.com>
2025-03-05 19:11:29 -0500

    NavBar Looking good

--- src/component/NavMenu/NavMenu.jsx
+++ src/component/NavMenu/NavMenu.jsx
@@ -5,14 +5,14 @@ function NavMenu() {
   return (
     <Container fluid className="d-flex align-items-center py-0 p-0 m-0 custom-navbar">
       <img src="/images/actual-logo.png" alt="Site Logo" width="232" />
-      <div className="d-flex w-100">
-        <Button variant="outline-info" size="lg" className="w-33 py-5 mb-0 m-0 button-text no-round">
+      <div className="d-flex flex-grow-1">
+        <Button variant="outline-info" size="lg" className="flex-grow-1 py-5 mb-0 m-0 button-text no-round">
           CODING
         </Button>
-        <Button variant="outline-success" size="lg" className="w-33 py-5 mb-0 m-0 button-text no-round">
+        <Button variant="outline-success" size="lg" className="flex-grow-1 py-5 mb-0 m-0 button-text no-round">
           COMICS
         </Button>
-        <Button variant="outline-danger" size="lg" className="w-33 py-5 mb-0 m-0 button-text no-round">
+        <Button variant="outline-danger" size="lg" className="flex-grow-1 py-5 mb-0 m-0 button-text no-round">
           CONTACT
         </Button>
       </div>
