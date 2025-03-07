b5ccb6dd35a97a88695c0569c313521834789e93
matt styles <styles.matt135@gmail.com>
2025-03-07 10:37:38 -0500

    Navmenu now collapses properly

--- src/component/NavMenu/NavMenu.jsx
+++ src/component/NavMenu/NavMenu.jsx
@@ -6,13 +6,28 @@ function NavMenu() {
     <Container fluid className="d-flex align-items-center py-0 p-0 m-0 custom-navbar">
       <img src="/images/actual-logo.png" alt="Site Logo" width="232" />
       <div className="d-flex flex-grow-1">
-        <Button variant="outline-info" size="lg" className="flex-grow-1 py-5 mb-0 m-0 button-text no-round">
+        <Button 
+          variant="outline-info" 
+          size="lg" 
+          className="flex-grow-1 py-5 mb-0 m-0 button-text no-round" 
+          style={{ height: "100%" }}
+        >
           CODING
         </Button>
-        <Button variant="outline-success" size="lg" className="flex-grow-1 py-5 mb-0 m-0 button-text no-round">
+        <Button 
+          variant="outline-success" 
+          size="lg" 
+          className="flex-grow-1 py-5 mb-0 m-0 button-text no-round" 
+          style={{ height: "100%" }}
+        >
           COMICS
         </Button>
-        <Button variant="outline-danger" size="lg" className="flex-grow-1 py-5 mb-0 m-0 button-text no-round">
+        <Button 
+          variant="outline-danger" 
+          size="lg" 
+          className="flex-grow-1 py-5 mb-0 m-0 button-text no-round" 
+          style={{ height: "100%" }}
+        >
           CONTACT
         </Button>
       </div>
