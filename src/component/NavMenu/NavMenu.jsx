f5bb4822a33913b9d626b8d894ba2841d1169bbc
matt styles <styles.matt135@gmail.com>
2025-03-09 14:00:35 -0400

    Styling looking better

--- src/component/NavMenu/NavMenu.jsx
+++ src/component/NavMenu/NavMenu.jsx
@@ -1,10 +1,17 @@
 import NavButton from "./NavButton";
+import { Link } from "react-router-dom";
+import logo from "../images/actual-logo.png";
+
 
 const NavMenu = () => {
   return (
     <nav className="navmenu">
       <ul className="nav-links">
-        <NavButton to="/" className="nav-logo">Logo</NavButton>
+        <li className="nav-logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" className="logo-img" />
+          </Link>
+        </li>
         <NavButton to="/coding" className="nav-coding">Coding</NavButton>
         <NavButton to="/comics" className="nav-comics">Comics</NavButton>
         <NavButton to="/contact" className="nav-contact">Contact</NavButton>
