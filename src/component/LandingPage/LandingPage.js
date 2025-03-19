27ef4d96101d88031175dbc9a8a243e135f90e39
matt styles <styles.matt135@gmail.com>
2025-03-19 15:58:05 -0400

    Some last min touches on landing

--- src/component/LandingPage/LandingPage.js
+++ src/component/LandingPage/LandingPage.js
@@ -10,14 +10,11 @@ const LandingPage = () => {
     <div className="content">
       {/* Welcome Container */}
         <h1 className="welcome-blurb">welcome to mattstyles.com</h1>
-        <h2 className="welcome-blurb2">I write code and comics.</h2>
+        <h2 className="welcome-blurb2">I write comics and code.</h2>
 
         {/* Profile Container */}
         <div className="profile">
-        <p className="profile-blurb">Thanks for visiting my site! I'm a Knoxville-based creative with skills in both web development 
-          and creative writing. As a certified front-end and back-end developer, I'm driven to build clean, user and collaborator-friendly 
-          websites. You can take a shortcut to my portfolio via the <Link to="/coding" className="bold-link">coding</Link> tab. 
-          I have published two graphic novels with more on the way. Check out my books under the <Link to="/comics" className="bold-link">comics</Link> tab. Please feel free to reach out 
+        <p className="profile-blurb">Thanks for visiting my site! I'm a Knoxville-based creative with skills in both web development and creative writing. As a certified front-end and back-end developer, I'm driven to build clean, user and collaborator-friendly websites. You can take a shortcut to my portfolio via the <Link to="/coding" className="bold-link">coding</Link> tab. I have also published two sci-fi graphic novels with more on the way! Check out my books under the <Link to="/comics" className="bold-link">comics</Link> tab. Please feel free to reach out 
           with any inquiries via the <Link to="/contact" className="bold-link">contacts</Link> tab. From my dog Remy and I, have a wonderful day!</p> 
           <img 
             src={profile} 
