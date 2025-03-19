beebe101a25c75802d1240a2303b87c679e081a8
matt styles <styles.matt135@gmail.com>
2025-03-19 16:08:16 -0400

    Functionally links to github repos

--- src/component/CodingPage/CodingPage.js
+++ src/component/CodingPage/CodingPage.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CodingPage() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.github.com/users/mstyles1/repos")
+      .then(response => setRepos(response.data))
+      .catch(error => console.error("Error fetching repos:", error));
+  }, []);
+  
   return (
-    <div>CodingPage</div>
-  )
+    <div>
+      <h2>My GitHub Portfolio</h2>
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.id}>
+            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+              {repo.name}
+            </a>
+            <p>{repo.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  
 }
