f23a42daa8e2d2ad53d8d54ff2bdb5867960c2ba
matt styles <styles.matt135@gmail.com>
2025-03-21 11:30:30 -0400

    Navmenu hamburger

--- src/component/NavMenu/NavMenu.jsx
+++ src/component/NavMenu/NavMenu.jsx
@@ -1,23 +1,41 @@
-import NavButton from "./NavButton";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import NavButton from "./NavButton";
+import { FiMenu, FiX } from "react-icons/fi"; // Import menu icons
 
 const NavMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navmenu">
-      <ul className="nav-links">
-        <li className="nav-logo">
+      <div className="nav-container">
+        {/* Logo */}
+        <div className="nav-logo">
           <Link to="/">
             <img src="/images/M-logo.png" alt="Logo" className="logo-img" />
           </Link>
-        </li>
-        <NavButton to="/coding" className="nav-coding">CODING</NavButton>
-        <NavButton to="/comics" className="nav-comics">COMICS</NavButton>
-        <NavButton to="/contact" className="nav-contact">CONTACT</NavButton>
-      </ul>
+        </div>
+
+        {/* Hamburger Button */}
+        <button className="hamburger" onClick={toggleMenu}>
+          {isOpen ? <FiX /> : <FiMenu />}
+        </button>
+
+        {/* Menu Links */}
+        <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+          <NavButton to="/coding" className="nav-coding">CODING</NavButton>
+          <NavButton to="/comics" className="nav-comics">COMICS</NavButton>
+          <NavButton to="/contact" className="nav-contact">CONTACT</NavButton>
+        </ul>
+      </div>
     </nav>
   );
 };
 
 export default NavMenu;
+
 
